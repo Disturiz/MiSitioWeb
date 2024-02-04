d50cf8f23345f271389a9d0a4a6e0938c2f9a8b1
Douglas Isturiz <disturiz362@gmail.com>
2024-02-04 15:49:19 -0600

    Proyecto Integrado

---
+++ SitioWebDouglas/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SitioWebDouglas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
